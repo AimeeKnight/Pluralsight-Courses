fc342e1acf1ef68c57e71c50273889fe2f62a73a
Aimee Knight <AimeeMarieKnight@gmail.com>
2014-11-15 17:59:25 -0500

    Update 1-events.js

--- node/3-node-intro-m3-streams-exercise-files/1-events.js
+++ node/3-node-intro-m3-streams-exercise-files/1-events.js
@@ -1,13 +1,21 @@
+// publish/subcribe (vs request/result)
+// diff: subscribe (call 'on') to a single event multiple times by providing multiple functions as an argument to on()
+// diff: errors are emitted as seperate events - not as first callback function args. Allows access to partial results
+
 var EventEmitter = require('events').EventEmitter;
 
 var getResource = function(c) {
     var e = new EventEmitter();
     process.nextTick(function() {
         var count = 0;
+        // publisher
+        // event is a string which can be any value
         e.emit('start');
         var t = setInterval(function () {
             e.emit('data', ++count);
             if (count === c) {
+                // events can be emitted with zero or more args
+                // emitter pattern A: a return value from a function call
                 e.emit('end', count);
                 clearInterval(t);
             }
@@ -16,8 +24,10 @@ var getResource = function(c) {
     return(e);
 };
 
+// event emitter instance
 var r = getResource(5);
 
+// subscriber
 r.on('start', function() {
     console.log("I've started!");
 });
