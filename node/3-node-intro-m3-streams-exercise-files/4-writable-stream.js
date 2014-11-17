996b1c0261010273bfde23c3d0c37ece521c232e
Aimee Knight <AimeeMarieKnight@gmail.com>
2014-11-16 19:22:21 -0500

    Update 4-writable-stream.js

--- node/3-node-intro-m3-streams-exercise-files/4-writable-stream.js
+++ node/3-node-intro-m3-streams-exercise-files/4-writable-stream.js
@@ -1,4 +1,6 @@
+// inspect stream's boolean property 'writable' to see if it's currently writable
 console.log("stdout is writable? " + process.stdout.writable);
 
+// process module includes several streams; stdin, stout, stderr
 process.stdout.write("Hello");
 process.stdout.write("World");
